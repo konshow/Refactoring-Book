c62cb37c70a3cb88d8c7c3eb653c1a1f968f6d43
konshow <chunpong.p@gmail.com>
2015-06-29 20:57:57 +0700

    first

---
+++ ExtractMethod001.java
@@ -0,0 +1,6 @@
+    void printOwing(double amount) {
+        printBanner();
+        //print details
+        System.out.println("name:" + _name);
+        System.out.println("amount" + amount);
+    }
